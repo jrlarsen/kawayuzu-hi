8e428769f8cd7a44c351114b8e8dea97c03b7ea5
John Larsen <steve@bluestamptravel.com>
2026-05-11 16:42:57 +0100

    Docs

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
-import clearConsole from 'clear-any-console';
+import welcome from 'cli-welcome';
+import pkgJSON from './package.json' with { type: "json" };
 
-clearConsole();
+welcome({
+   title: pkgJSON.name,
+   tagLine: 'A friendly app',
+   description: pkgJSON.description,
+   version: pkgJSON.version,
+   bgColor: '#fadc00',
+   color: '#000000',
+   bold: true,
+   clear: true,
+});
+
 console.log(`
 Hi
 
---
+++ readme.md
@@ -0,0 +1,21 @@
+# kawayuzu-cli
+
+A super-simple test package to say hi on the console.
+
+## Installation
+
+```shell
+npm i kawayuzu-cli
+```
+
+## Usage
+
+### After installation
+```shell
+kawayuzu-hi
+```
+
+### Without first installing
+```shell
+npx kawayuzu-cli
+```
